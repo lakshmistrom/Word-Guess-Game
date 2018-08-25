92c993aa894143f606a0a6d825a6e631eb3ff105
Sweet Ace <lakshmialeman@gmail.com>
2018-08-24 23:18:23 -0700

    finished making changes to get the object set up

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Word-Guess-Game
 Homework 3 assignment
+
+We were tasked with creating a hangman game and chose to do a retro style user interface.
+
+My high level pseudocode started here:
+
+1. Press any key to get started!!!
+1. Display the word to be guessed by giving the correct amount of empty spaces
+1. As the letters are guessed correctly replace the blank spaces with the guessed letters
+1. When the user guesses the wrong letter the hangman starts to be drawn piece by piece
+1. The wrong set of letters guessed by the user will be displayed while game is in place
+1. Wins is the number of times the user guessed the word correctly losses 
+1. Losses is when the hangman appears with both eyes crossed out
+1. Whether the user loses or wins display the next word to be guessed to the user
+
+I also took the liberty to leave the comments explaining what each line does inside the (./assets/javascript/game.js).
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,48 @@
-// list of artists to be guessed by user
-var artistsToBeGuessed = ["pink", "rihanna", "beyonce", "katy perry", "bruno mars", "kesha", "maroon 5", "miley cyrus", "spice girls", "shakira", "britney spears", "madonna", "demi lovato", "adele", "carly rae", "sia", "gwen stefani", "alicia keys", "john legend", "fergie", "ed sheran", "lady gaga", "the black eyed peas", "ismael kamawool"];
+//object build
+function Hangman() {
+  //initializes list of artists to be guessed by user
+  this.artistsToBeGuessed = ["pink", "rihanna", "beyonce", "katy perry", "bruno mars", "kesha", "maroon 5", "miley cyrus", "spice girls", "shakira", "britney spears", "madonna", "demi lovato", "adele", "carly rae", "sia", "gwen stefani", "alicia keys", "john legend", "fergie", "ed sheran", "lady gaga", "the black eyed peas", "ismael kamawool"];
 
-//hangman images
-var hangmanImages = ["assets/images/hangman-1.png", "assets/images/hangman-2.png", "assets/images/hangman-3.png", "assets/images/hangman-4.png", "assets/images/hangman-5.png", "assets/images/hangman-6.png", "assets/images/hangman-7.png", "assets/images/hangman-8.png", "assets/images/hangman-9.png"];
+  //initializes images for the hangman figure
+  this.hangmanImages = ["assets/images/hangman-1.png", "assets/images/hangman-2.png", "assets/images/hangman-3.png", "assets/images/hangman-4.png", "assets/images/hangman-5.png", "assets/images/hangman-6.png", "assets/images/hangman-7.png", "assets/images/hangman-8.png", "assets/images/hangman-9.png"];
 
-//initializes string of wrong used letters
-var usedLetters = "";
+  //initializes letters that were guessed wrong
+  this.usedLetters = "";
 
-//initializes artist to be guessed from the array of artistsToBeGuessed
-var artistToGuess = "";
+  //initializes the artist that will be guessed by the user
+  this.artistToGuess = "";
 
-//represents the word that was converted to underscores and spaces
-var wordToGuess = "";
+  //initializes the underscores and spaces string
+  this.wordToGuess = "";
 
-//represents the number of times the user wins by guessing the word
-var wins = 0;
+  //initializes wins counter
+  this.wins = 0;
 
-//initializes the audio file to be played once a round is won
-var audioWins = new Audio("assets/music/171670__fins__success-2.wav");
+  //initializes audio file for the won rounds
+  this.audioWins = new Audio("assets/music/171670__fins__success-2.wav");
 
-//represents the number of times the user lost by guessing the wrong word
-var losses = 0;
+  //initializes the number of times there is a loss
+  this.losses = 0;
 
-//initializes the audio file to be placed once a round is lost
-var audioLosses = new Audio("assets/music/364929__josepharaoh99__game-die.mp3");
-
-//initialize data to be used by the key up event
-init();
-
-//function is run whenever the user presses a key
-document.onkeyup = function displayChoices(event) {
-  //determines which key is pressed
-  var userGuess = event.key.toLowerCase();
-  //ignore lost game before refresh happens to load new word
-  if(losses >= 8){
-    return;
-  }
-  //ignore bad input
-  if (userGuess.length === 1 && userGuess.match(/([a-z0-9 ])/g)) {
-console.log(userGuess);
-    //handles guessin wrong/right letters guess(userGuess)
-    //if the user has succesfully put in all the characters needed to guess the word increase wins by one
-    if (guess(userGuess) === artistToGuess) {
-      //initialize data with a delay
-      setTimeout(init,3000);
-
-      //increment wins
-      wins++;
-
-      // and update wins in the html
-      displayEveryWin.textContent = wins;
-
-      //output audio file
-      audioWins.play();
-    } else if(losses >= 8){
-      //initialize data with a delay
-      setTimeout(init,3000);
-      
-      //output audio file
-      audioLosses.play();
-    }
-    //if the user has run out of guesses the user has lost, all the hangman images are on the screen, and there would be a new word to be guessed presented
-  }
-};
-
-//helper functions
-
-//initializes data
-function init() {
+  //initializes the music file to be played when the round is lost
+  this.audioLosses = new Audio("assets/music/364929__josepharaoh99__game-die.mp3");
+}
+//initializing data to be used for the hangman game
+Hangman.prototype.init = function () {
+  //initializes data
   //holds string of wrong used letters
-  usedLetters = "";
+  this.usedLetters = "";
 
   //initializes the losses
-  losses = 0;
+  this.losses = 0;
 
   //choose an artist at random from the artistsToBeGuessedArray 
-  artistToGuess = artistsToBeGuessed[Math.floor(Math.random() * artistsToBeGuessed.length)];
+  this.artistToGuess = this.artistsToBeGuessed[Math.floor(Math.random() * this.artistsToBeGuessed.length)];
 
   //represents the word that was converted
-  wordToGuess = artistToHyphen(artistToGuess);
+  this.wordToGuess = artistToUnderScore(this.artistToGuess);
 
-  //display hyphens to user
+  //display UnderScores to user
   var emptyGuess = document.getElementById("displayProgress");
 
   // display letters already used to the user
@@ -96,46 +58,46 @@ function init() {
   var hangmanRemains = document.getElementById("hangman");
 
   //load the word to be guessed
-  displayProgress.textContent = wordToGuess;
+  displayProgress.textContent = this.wordToGuess;
 
   //load letters that were already guessed in the html
-  displayAlreadyUsedLetters.textContent = usedLetters;
+  displayAlreadyUsedLetters.textContent = this.usedLetters;
 
   //load wins to the html
-  displayEveryWin.textContent = wins;
+  displayEveryWin.textContent = this.wins;
 
   //load guesses remaining to the html
-  displayGuessesRemaining.textContent = 8 - losses;
+  displayGuessesRemaining.textContent = 8 - this.losses;
 
   //load hangman's first image
-  hangman.src = hangmanImages[0];
-}
+  hangman.src = this.hangmanImages[0];
+
+};
 
 //handles choosing between right guesses and wrong guesses
-function guess(userGuess) {
-
+Hangman.prototype.guess = function(userGuess) {
   //set up flag for wrong guesses
   var wrongGuessMade = false;
 
   //makes the right guess
-  if (artistToGuess.indexOf(userGuess) !== -1) {
+  if (this.artistToGuess.indexOf(userGuess) !== -1) {
     //userGuess is the guess input by the user
-    //hyphenString is the current string holding hyphens and spaces
+    //wordToGuess is the current string holding underScores and spaces
     //artistToGuess represents the chosen artist to be guesssed
     //display correct letters
-    var revealArtist = displayUnderScoresConvertedBack(userGuess, wordToGuess, artistToGuess);
+    var revealArtist = displayUnderScoresConvertedBack(userGuess, this.wordToGuess, this.artistToGuess);
 
     //update user guess in the html
     displayProgress.textContent = revealArtist;
 
     //load the user guess back in
-    wordToGuess = revealArtist;
+    this.wordToGuess = revealArtist;
 
   } else {
     //makes wrong guess
 
     //display wrong displayed letters
-    var wrongGuessedLetters = wrongGuesses(userGuess, artistToGuess, usedLetters);
+    var wrongGuessedLetters = wrongGuesses(userGuess, this.artistToGuess, this.usedLetters);
 
     //update flag
     wrongGuessMade = true;
@@ -144,39 +106,85 @@ function guess(userGuess) {
     displayAlreadyUsedLetters.textContent = wrongGuessedLetters;
 
     //update usedLetters
-    usedLetters = wrongGuessedLetters;
+    this.usedLetters = wrongGuessedLetters;
 
     // as the user guesses the wrong letter the hangman starts to be drawn piece by piece--------
     if (wrongGuessMade === true) {
-      console.log(losses);
       //increments losses
-      losses++;
-      console.log(losses);
-      //decrease number of guesses remaining
+      this.losses++;
 
+      //decrease number of guesses remaining
       //update html with guesses remaining
-      displayGuessesRemaining.textContent = 8 - losses;
+      displayGuessesRemaining.textContent = 8 - this.losses;
 
       //find hangman image to associate with number of guessees remaining
-      hangman.src = hangmanImages[losses];
-
+      hangman.src = this.hangmanImages[this.losses];
     }
   }
 
   //return string with the guessed artist
   return revealArtist;
-}
+};
 
+//handle user guess entries
+Hangman.prototype.entries = function(userGuess){
+  //ignore lost game before refresh happens to load new word
+  if (this.losses >= 8) {
+    return;
+  }
+  //ignore bad input
+  if (userGuess.length === 1 && userGuess.match(/([a-z0-9 ])/g)) {
+    //handles guessin wrong/right letters guess(userGuess)
+    //if the user has succesfully put in all the characters needed to guess the word increase wins by one
+    if (this.guess(userGuess) === this.artistToGuess) {
+      //initialize data with a delay
+      setTimeout(this.init.bind(this), 3000);
+
+      //increment wins
+      this.wins++;
+
+      // and update wins in the html
+      displayEveryWin.textContent = this.wins;
+
+      //output audio file
+      this.audioWins.play();
+    } else if (this.losses >= 8) {
+      //initialize data with a delay
+      setTimeout(this.init.bind(this), 3000);
+
+      //output audio file
+      this.audioLosses.play();
+    }
+    //if the user has run out of guesses the user has lost, all the hangman images are on the screen, and there would be a new word to be guessed presented
+  }
+};
+//initialize data to be used by the key up event
+var hangmanGame = new Hangman();
+
+//initialize data
+hangmanGame.init();
+
+
+//function is run whenever the user presses a key
+document.onkeyup = function displayChoices(event) {
+  //determines which key is pressed
+  var userGuess = event.key.toLowerCase();
+
+  //handles user entries
+  hangmanGame.entries(userGuess);
+};
+
+//helper functions
 //output the string of wrong guessed letters
 function wrongGuesses(userGuess, artistToGuess, usedLetters) {
   //return the letters that don't match the artistToGuess
   return usedLetters + userGuess;
 }
 
-//As the letters are guessed correctly replace the hyphens with letters that were used by the user
-function displayUnderScoresConvertedBack(userGuess, hyphenString, artistToGuess) {
-  //turn hyphenString into array
-  var tempArray = hyphenString.split("");
+//As the letters are guessed correctly replace the underScores with letters that were used by the user
+function displayUnderScoresConvertedBack(userGuess, underScoreString, artistToGuess) {
+  //turn underScoreString into array
+  var tempArray = underScoreString.split("");
 
   //need to iterate over the artistToGuess to find if the letter 
   //that was input by the user is there
@@ -184,7 +192,7 @@ function displayUnderScoresConvertedBack(userGuess, hyphenString, artistToGuess)
 
     //if the userGuess equals the character at that index
     if (userGuess === artistToGuess[i]) {
-      //add back in the userGuess at that index in hyphenString
+      //add back in the userGuess at that index in underScoreString
       tempArray[i] = userGuess;
     }
   }
@@ -192,21 +200,19 @@ function displayUnderScoresConvertedBack(userGuess, hyphenString, artistToGuess)
   //turn array back to a string
   var guessedLettersIn = tempArray.join("");
 
-  //returns string of characters and hyphens
+  //returns string of characters and underScores
   return guessedLettersIn;
 }
 
-//go through each element of the array and count how many hyphens are needed to be displayed before the user starts guessing the word
-function artistToHyphen(artistToGuess) {
-  // checks artist that was chosen randomly 
-  console.log(artistToGuess);
+//go through each element of the array and count how many underScores are needed to be displayed before the user starts guessing the word
+function artistToUnderScore(artistToGuess) {
 
-  //create a new string to hold hyphens based on the number of characters found in the string
-  var hyphenString = "";
+  //create a new string to hold underScores based on the number of characters found in the string
+  var underScoreString = "";
 
   //go through the artistToGuess string and count how many characters are inside then convert it to underscores
-  hyphenString = artistToGuess.replace(/[a-z0-9]/g, "_");
+  underScoreString = artistToGuess.replace(/[a-z0-9]/g, "_");
 
-  //returns new string of hyphens and spaces
-  return hyphenString;
+  //returns new string of underScores and spaces
+  return underScoreString;
 };
